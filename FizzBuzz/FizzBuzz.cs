f1981c426a19c3ce5f34fa642c72af20d10d2fa1
PabloTejGar <pablo.tejgar@gmail.com>
2023-03-17 21:18:59 +0100

    Green:AddMultiple3And5ReturnsFizzBuzz

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -8,6 +8,10 @@
 
         public string FizzBuzz(int number)
         {
+            if(number %3 == 0 && number %5 == 0)
+            {
+                return "FizzBuzz";
+            }
             if(number % 3 == 0)
             {
                 return "Fizz";
