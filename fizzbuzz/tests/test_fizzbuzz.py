65ba994861196a8851620ee71531b4ab61c8d095
Pablo <pablo.tejgar@gmail.com>
2023-08-03 12:57:43 +0200

    refactor:simplify test

--- fizzbuzz/tests/test_fizzbuzz.py
+++ fizzbuzz/tests/test_fizzbuzz.py
@@ -5,22 +5,14 @@ from src.fizzbuzz import FizzBuzz
 
 class TestFizzBuzz:
 
-    @pytest.mark.parametrize("test_input,expected", [(1, "1"), (2, "2"), (4, "4")])
+    @pytest.mark.parametrize("test_input,expected",
+                             [(1, "1"), (2, "2"), (3, "Fizz"),
+                              (4, "4"), (5, "Buzz"), (6, "Fizz"),
+                              (7,"7"), (8, "8"), (9, "Fizz"),
+                              (10, "Buzz"), (12, "Fizz"), (20, "Buzz")
+                              ]
+                             )
     def test_number_is_returned(self, test_input, expected):
-
-        fizzbuzz_processor = FizzBuzz()
-        assert expected == fizzbuzz_processor.process(test_input)
-
-    @pytest.mark.parametrize("test_input,expected", [(3, "Fizz"), (6, "Fizz"), (9, "Fizz"), (12, "Fizz")])
-
-    def test_multiple_of_three_provided_then_fizz_returned(self, test_input, expected):
-
-        fizzbuzz_processor = FizzBuzz()
-        assert expected == fizzbuzz_processor.process(test_input)
-
-    @pytest.mark.parametrize("test_input,expected", [(5, "Buzz"), (10, "Buzz"), (20, "Buzz"), (25, "Buzz")])
-
-    def test_multiple_of_five_provided_then_buzz_returned(self, test_input, expected):
 
         fizzbuzz_processor = FizzBuzz()
         assert expected == fizzbuzz_processor.process(test_input)
