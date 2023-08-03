a021becd140eeedd5412914048876367a7b42c4c
Pablo <pablo.tejgar@gmail.com>
2023-08-03 12:41:54 +0200

    green:test_six_provided_then_fizz_returned

--- fizzbuzz/src/fizzbuzz.py
+++ fizzbuzz/src/fizzbuzz.py
@@ -3,4 +3,6 @@ class FizzBuzz:
     def process(self, number: int):
         if number == 3:
             return "Fizz"
+        if number == 6:
+            return "Fizz"
         return str(number)
