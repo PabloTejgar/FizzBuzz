d25dca81479d3a2ddd55a385b4c7c1a1f49283fe
Pablo <pablo.tejgar@gmail.com>
2023-08-03 12:20:37 +0200

    green:test_fourth_number_is_returned

--- fizzbuzz/tests/test_fizzbuzz.py
+++ fizzbuzz/tests/test_fizzbuzz.py
@@ -12,3 +12,7 @@ class TestFizzBuzz:
         fizzbuzz_processor = FizzBuzz()
         assert "2" == fizzbuzz_processor.process(2)
 
+    def test_fourth_number_is_returned(self):
+        fizzbuzz_processor = FizzBuzz()
+        assert "4" == fizzbuzz_processor.process(4)
+
