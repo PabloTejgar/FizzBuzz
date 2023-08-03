f0315de124582f0138442bff6c01fc302abcf9ff
Pablo <pablo.tejgar@gmail.com>
2023-08-03 12:53:57 +0200

    green:test_multiple_of_five_provided_then_buzz_returned

--- fizzbuzz/src/fizzbuzz.py
+++ fizzbuzz/src/fizzbuzz.py
@@ -3,5 +3,7 @@ class FizzBuzz:
     def process(self, number: int):
         if number % 3 == 0:
             return "Fizz"
+        if number % 5 == 0:
+            return "Buzz"
 
         return str(number)
