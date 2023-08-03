c6f7c28852e4eb61abc2d9bb2e694e7e810c8c3f
Pablo <pablo.tejgar@gmail.com>
2023-08-03 13:04:56 +0200

    red:test_fizzbuzz_is_returned

--- fizzbuzz/tests/test_fizzbuzz.py
+++ fizzbuzz/tests/test_fizzbuzz.py
@@ -8,14 +8,20 @@ class TestFizzBuzz:
     @pytest.mark.parametrize("test_input,expected",
                              [(1, "1"), (2, "2"), (3, "Fizz"),
                               (4, "4"), (5, "Buzz"), (6, "Fizz"),
-                              (7,"7"), (8, "8"), (9, "Fizz"),
+                              (7, "7"), (8, "8"), (9, "Fizz"),
                               (10, "Buzz"), (12, "Fizz"), (20, "Buzz")
                               ]
                              )
-    def test_number_is_returned(self, test_input, expected):
+    def test_valid_output_is_returned(self, test_input, expected):
 
         fizzbuzz_processor = FizzBuzz()
         assert expected == fizzbuzz_processor.process(test_input)
+
+    def test_fizzbuzz_is_returned(self, test_input, expected):
+
+        fizzbuzz_processor = FizzBuzz()
+        assert "FizzBuzz" == fizzbuzz_processor.process(15)
+
 
 
 
